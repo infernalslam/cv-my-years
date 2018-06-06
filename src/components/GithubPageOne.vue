6bd24e389059266dec543f0d993acb5d06cbdb4a
TAK <infernal-slam@hotmail.com>
2018-06-07 00:37:28 +0700

    add page

--- src/components/GithubPageOne.vue
+++ src/components/GithubPageOne.vue
@@ -3,12 +3,12 @@
     <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">
-            Full Height title
-          </h1>
-          <h2 class="subtitle">
-            Full Height subtitle
-          </h2>
+          <div class="columns is-mobile">
+            <div class="column line-height">
+              <h1> 567k </h1>
+              <h1 class="has-text-white-ter">lines of code</h1>
+            </div>
+          </div>
         </div>
       </div>
     </section>
